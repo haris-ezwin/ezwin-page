8535b99f326b5ddb6b010fc1aa938d21463850bc
Haris Samingan <haris@ezwin.academy>
2026-01-16 10:50:58 +0800

    Update Astro configuration for Vercel deployment and package dependencies
    - Added Vercel serverless adapter to the Astro configuration for hybrid output.
    - Updated package dependencies, including upgrading Astro, MDX, and other related packages to their latest versions.
    - Introduced new dependencies and updated existing ones in package-lock.json for improved functionality and compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  output: "hybrid",
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
